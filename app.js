950d31ae1cb8b114cdf80aec07176ef0b59e4922
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2024-05-08 23:22:03 +0500

    header complete

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const searchInput = document.getElementById('searchInput');
+const cityName = document.getElementById('searchInput').value;
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3e57e7e011029d2fb7abace9bdd9e5ec}')
-.then
+
+const p = fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=5e8cb2874330f87f5695ebbb128be484&q=karachi`)
+.then((response) =>{
+    console.log(response)
+    return response.json()
+})
+.then((response) =>{
+    console.log(response);
+})
