431ebf9fb84c15f1e350ebee540ce01b4859c760
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2024-05-10 16:52:05 +0500

    add some function in getWeather data

--- app.js
+++ app.js
@@ -12,16 +12,31 @@ const temp = document.getElementById('temp'),
         pressure = document.getElementById('pressure'),
         visibility = document.getElementById('visibilty'),
         sunrise = document.getElementById('sunrise'),
-        sunset = document.getElementById('sunset')
+        sunset = document.getElementById('sunset'),
+        cityName = document.getElementById('searchInput')
+
+
+        let currentTime = new Date();
+        let currentHour = currentTime.getHours()
+
+        
+cityName.addEventListener('keypress' ,(e) =>{
+    if(e.key === 'Enter'){
+        getWeather()
+        cityName.value = ''
+    }
+})
 
 const getWeather = () =>{
+    let cityNameValue = document.getElementById('searchInput').value || "karachi";
 
-    const p = fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=5e8cb2874330f87f5695ebbb128be484&q=karachi`)
+    const p = fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=5e8cb2874330f87f5695ebbb128be484&q=${cityNameValue}`)
     .then((response) =>{
-        console.log(response)
+        // console.log(response)
         return response.json()
     })
     .then((response) =>{
+        console.log(response)
         let temprature = Math.round(response.main.temp)
         let feelLike = Math.round(response.main.feels_like)
         temp.innerText = temprature
@@ -37,17 +52,24 @@ const getWeather = () =>{
         pressure.innerText = ` ${response.main.pressure} mBar`
         let visibilityinKilometer = response.visibility / 1000;
         visibility.innerHTML = `${visibilityinKilometer} km`
-        // let convertTime = (timestamp , timeZone) => {
-        //     let date = new Date(timestamp * timeZone) / 1000;
-        //     return date.toLocaleString([], { hour: '2-digit', minute: '2-digit', hour12: true });
-        // }
-        // let sunriseTime = convertTime(response.sys.sunrise , response.timezone)
-        // let sunsetTime = convertTime(response.sys.sunset , response.timezone)
-        // console.log(sunriseTime , sunsetTime);
-        // sunrise.innerText = `${sunriseTime} am`
-        // sunset.innerText = `${sunsetTime} pm`
+        
+        let sunriseDate = new Date(response.sys.sunrise * 1000)
+        let sunsetDate = new Date(response.sys.sunset * 1000)
+        sunrise.innerText = `${moment(sunriseDate).format("h:mm")} am`;
+        sunset.innerText =` ${moment(sunsetDate).format("h:mm")} pm`;
 
-        console.log(response , temprature , locationName);
+        
+        
+
+        
+         
+
+         
+
+         
+
+    })
+    .catch((err) =>{
+        console.log(err)
     })
 }
-getWeather()
